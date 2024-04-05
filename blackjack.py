394fafbb85f980d41b7084d974c3f46970f05971
anuj8848 <anujgiri8848@gmail.com>
2024-04-06 00:54:10 +0545

    this is second commit after adding first testing. further modification should be done in tap_or_hold function

--- blackjack.py
+++ blackjack.py
@@ -12,7 +12,28 @@ def check_n_choose_A(checklists):
             checklists[i]=get_value_of_A
 
     return checklists
-    
+ 
+def tap_or_hold(player):
+    get_resonse=input("Tap or Hold (t/h)? ")
+    if get_resonse=='t':
+        player["cards"].append(choosefrom(deck))
+        player["cards"]=check_n_choose_A(player["cards"])
+
+def results(player,dealer):
+    if sum(player["cards"])==21 and sum(dealer['cards']) !=21:
+        print("Your won !!!!!!")
+        player['coins']=1.5*player['coins']
+    elif sum(player["cards"])==21 and sum(dealer['cards']) ==21:
+        print("Draw!!!!!!")
+    elif sum(player["cards"]) >21:
+        print(f"player Brusted!! {player['name']} looses!!!!!!!!1")
+        player['coins']-=player['bet']
+    elif sum(player['cards']) >sum(dealer['cards']):
+        print("you won!!")
+        player['coins']=2*player['coins']
+
+    print(f"remaining coins:{player['coins']}")
+
 
 def blackjack():
     player['cards']=[]
@@ -37,17 +58,21 @@ def blackjack():
     print(player["cards"])
     print(dealer["cards"])
         
-    # if player["cards"].sum==21:
-    #     print("BLackjack!!! You won!!!!!") 
-    # else
-           
+    if sum(player["cards"])==21:
+        print("BLackjack!!! You won!!!!!") 
+    
+    else:
+        while sum(player["cards"]) <21:
+            tap_or_hold(player)
+            results(player,dealer)
+         
+while True:
+    gamble=input('You want to play BlackJack(y/n)?: ')
+    if gamble=='y':
+        blackjack()
+    else:
+        break
+       
+
+    
   
-    
-            
-    
-
-blackjack()
-        
-        
-
-    
