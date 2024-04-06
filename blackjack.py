9dabd3f477783f8313af78aa0896b3e46b539d0d
anuj8848 <anujgiri8848@gmail.com>
2024-04-06 08:50:11 +0545

    blackjack game without options for double and split

--- blackjack.py
+++ blackjack.py
@@ -14,25 +14,35 @@ def check_n_choose_A(checklists):
     return checklists
  
 def tap_or_hold(player):
-    get_resonse=input("Tap or Hold (t/h)? ")
-    if get_resonse=='t':
-        player["cards"].append(choosefrom(deck))
-        player["cards"]=check_n_choose_A(player["cards"])
+    while True:
+        get_resonse=input("Tap or Hold (t/h)? ")
+        if get_resonse=='t':
+            player["cards"].append(choosefrom(deck))
+            player["cards"]=check_n_choose_A(player["cards"])
+            print(player["cards"])
+            if sum(player['cards'])>=21:
+                break
+        else:
+            print(player['cards'])
+            break
+
 
 def results(player,dealer):
+    print(str(player["cards"]).center(100))
+    print(str(dealer["cards"]).center(100))
     if sum(player["cards"])==21 and sum(dealer['cards']) !=21:
-        print("Your won !!!!!!")
-        player['coins']=1.5*player['coins']
+        print("Your won !!!!!!".center(100))
+        player['coins']+=1.5*player['bet']
     elif sum(player["cards"])==21 and sum(dealer['cards']) ==21:
-        print("Draw!!!!!!")
+        print("Draw!!!!!!".center(100))
     elif sum(player["cards"]) >21:
-        print(f"player Brusted!! {player['name']} looses!!!!!!!!1")
+        print(f"player Brusted!! {player['name']} looses!!!!!!!!1".center(100))
         player['coins']-=player['bet']
     elif sum(player['cards']) >sum(dealer['cards']):
-        print("you won!!")
-        player['coins']=2*player['coins']
+        print("you won!!".center(100))
+        player['coins']+=2*player['bet']
 
-    print(f"remaining coins:{player['coins']}")
+    print(f"remaining coins:{player['coins']}".center(100))
 
 
 def blackjack():
@@ -46,25 +56,22 @@ def blackjack():
     get_bet=int(input("coins to bet: "))
     player["bet"]=get_bet
     
-    player["coins"]-=player["bet"]
-    
+   
     for i in range(2):
         player['cards'].append(choosefrom(deck))
         dealer['cards'].append(choosefrom(deck))
-    
-    player["cards"]=check_n_choose_A(player["cards"])
-    dealer["cards"]=check_n_choose_A(dealer["cards"])
 
+    player["cards"]=check_n_choose_A(player["cards"])
+
+    
     print(player["cards"])
-    print(dealer["cards"])
         
     if sum(player["cards"])==21:
         print("BLackjack!!! You won!!!!!") 
     
     else:
-        while sum(player["cards"]) <21:
-            tap_or_hold(player)
-            results(player,dealer)
+        tap_or_hold(player)
+        results(player,dealer)
          
 while True:
     gamble=input('You want to play BlackJack(y/n)?: ')
@@ -75,4 +82,3 @@ while True:
        
 
     
-  
